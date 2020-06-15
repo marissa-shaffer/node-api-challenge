a1d0ce86d3aa1f551046d9896c795e3a642c4ff7
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-14 20:28:43 -0500

    Post for /api/projects in project router

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -3,6 +3,21 @@ const projects = require("../helpers/projectModel")
 
 const router = express.Router()
 
-
+router.post("/", (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        return res.status(400).json({
+            message: "Please provide name and description for project"
+        })
+    }
+    projects.insert(req.body)
+    .then((project) => {
+        res.status(201).json(project)
+    })
+    .catch((error) => {
+        res.status(500).json({
+            message: "An error occured while saving your project"
+        })
+    })
+})
 
 module.exports = router
