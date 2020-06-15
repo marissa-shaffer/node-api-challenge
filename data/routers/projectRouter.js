15de4954f0b3ffe502ae056e27cd99aea5e6e6db
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-14 20:44:34 -0500

    Get /api/projects/:id/actions for project router

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -1,42 +1,58 @@
-const express = require("express")
-const projects = require("../helpers/projectModel")
+const express = require("express");
+const projects = require("../helpers/projectModel");
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/", (req, res) => {
-    if (!req.body.name || !req.body.description) {
-        return res.status(400).json({
-            message: "Please provide name and description for project"
-        })
-    }
-    projects.insert(req.body)
+  if (!req.body.name || !req.body.description) {
+    return res.status(400).json({
+      message: "Please provide name and description for project",
+    });
+  }
+  projects
+    .insert(req.body)
     .then((project) => {
-        res.status(201).json(project)
+      res.status(201).json(project);
     })
     .catch((error) => {
-        res.status(500).json({
-            message: "An error occured while saving your project"
-        })
-    })
-})
+      res.status(500).json({
+        message: "An error occured while saving your project",
+      });
+    });
+});
 
 router.get("/:id", (req, res) => {
-    projects.get(req.params.id)
+  projects
+    .get(req.params.id)
     .then((project) => {
-        if(project) {
-            res.status(200).json(project)
-        }else {
-            res.status(404).json({
-                message: "Project not found"
-            })
-        }
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "Project not found",
+        });
+      }
     })
     .catch((error) => {
-        console.log(error)
-        res.status(500).json({
-            message: "Error retrieving project"
-        })
-    })
-})
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving project",
+      });
+    });
+});
 
-module.exports = router
+router.get("/:id/actions", (req, res) => {
+  projects
+    .getProjectActions(req.params.id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).json({
+        message: "No actions for this project",
+      });
+    });
+});
+
+module.exports = router;
