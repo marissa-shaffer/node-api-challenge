953e33a7874e5f86d4680214a5de1e3ca4375e00
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-15 20:29:53 -0500

    Post api/projects/:id/actions for action router

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -3,7 +3,23 @@ const actions = require("../helpers/actionModel")
 
 const router = express.Router()
 
-
+router.post("/", (req, res) => {
+    if(!req.body.description || !req.body.notes) {
+        return res.status(400).json({
+            message: "Please provide notes and description for the action."
+        })
+    } 
+    actions.insert(req.body)
+        .then((action) => {
+            res.status(201).json(action)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "Error while saving"
+            })
+        })
+})
 
 
 module.exports = router
