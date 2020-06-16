6accf3bdbca913aa13b4986703d704ac8e61f3c8
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-15 21:25:55 -0500

    Post api/projects/:id/actions for actionRouter

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -21,5 +21,21 @@ router.post("/", (req, res) => {
         })
 })
 
+router.get("/:id", (req, res) => {
+    actions.get(req.params.id)
+    .then((action) => {
+        if(action){
+            res.status(200).json(action)
+        }else {
+            res.status(404).json({
+                message: "Action not found"
+            })
+        }
+    })
+    .catch((error) => {
+        next(error)
+    }) 
+})
+
 
 module.exports = router
